972c2d55cb32ed96d0258a3101595b025e7febe5
VarunVk <vijay094@umn.edu>
2018-02-27 09:12:34 -0600

    Updated rec6.c

--- discussion6/rec6.c
+++ discussion6/rec6.c
@@ -16,122 +16,134 @@
 
 void shallowCopy(char* old, char* new) {
 
-	// TODO: Create links to all files in old in new.
-	DIR *dir;
-	struct dirent *dp;
+    DIR *dir;
+    struct dirent *dp;
     struct stat Fstat;
 
+    // If create the new folder if does not exist
     if(!(stat(new, &Fstat) == 0 && S_ISDIR(Fstat.st_mode)))
         mkdir(new, 0700);
 
-	if ((dir = opendir(old)) == NULL) {
-        printf("Directory does not exist: %s\n", old);
+    if ((dir = opendir(old)) == NULL) {
+        printf("Directory %s does not exist\n", old);
         return;
     }
 
     while ((dp = readdir(dir)) != NULL)
     {
-		if (!strcmp(dp->d_name, ".") || !strcmp(dp->d_name, "..") || (dp->d_type == DT_DIR))
-			continue;
+        if (!strcmp(dp->d_name, ".") || !strcmp(dp->d_name, "..") || (dp->d_type == DT_DIR))
+            continue;
 
-		char dst_path[256], src_path[256];
+        char dst_path[256], src_path[256];
         sprintf(dst_path, "%s/%s", new, dp->d_name);
         sprintf(src_path, "%s/%s", old, dp->d_name);
-		printf("Copying %s ---> %s\n", src_path, dst_path);
-		if (link(src_path, dst_path) == -1) {
-			printf("Could not copy. Errno %d\n", errno);
-		}
-	}
+
+        if (link(src_path, dst_path) == -1) {
+            printf("Could not create the hard copy for %s. Errno %d\n",
+                   src_path, errno);
+        }
+    }
 }
 
 void deepCopy(char* old, char* new) {
 
-	// TODO: Copy the contents of all files in old to new.
-	DIR *dir;
-	struct dirent *dp;
+    DIR *dir;
+    struct dirent *dp;
     struct stat Fstat;
 
+    // If create the new folder if does not exist
     if(!(stat(new, &Fstat) == 0 && S_ISDIR(Fstat.st_mode)))
         mkdir(new, 0700);
 
-	if ((dir = opendir(old)) == NULL) {
+    if ((dir = opendir(old)) == NULL) {
         printf("Directory does not exist: %s\n", old);
         return;
     }
 
     while ((dp = readdir(dir)) != NULL)
     {
-		if (!strcmp(dp->d_name, ".") || !strcmp(dp->d_name, "..") || (dp->d_type == DT_DIR))
-			continue;
+        if (!strcmp(dp->d_name, ".") || !strcmp(dp->d_name, "..") || (dp->d_type == DT_DIR))
+            continue;
 
-		char dst_path[256], src_path[256];
-		struct stat src_stat;
-		lstat(src_path, &src_stat);
-		int src_size = (int) src_stat.st_size;
-		sprintf(src_path, "%s/%s", old, dp->d_name);
+        char dst_path[256], src_path[256];
+        sprintf(src_path, "%s/%s", old, dp->d_name);
         sprintf(dst_path, "%s/%s", new, dp->d_name);
-		printf("Copying %s (=%d) ---> %s\n", src_path, src_size, dst_path);
 
-		int src_fd = open(src_path, O_RDONLY);
-		int dst_fd = open(dst_path, O_WRONLY | O_CREAT);
+        struct stat src_stat;
+        lstat(src_path, &src_stat);
 
-		int size = 0;
-		if (S_ISLNK(src_stat.st_mode))
-		{
+        int src_fd;
+        if ((src_fd = open(src_path, O_RDONLY)) < 0)
+        {
+            printf("Skipping %s as it couldn't be opened. Errno %d\n", src_path, errno);
+            continue;
+        }
+        int dst_fd;
+        if ((dst_fd = open(dst_path, O_WRONLY | O_CREAT)) < 0)
+        {
+            printf("Skipping %s as it couldn't be opened. Errno %d\n", dst_path, errno);
+            continue;
+        }
+
+        int size = 0;
+        if (S_ISLNK(src_stat.st_mode))
+        {
+            int src_size = (int) src_stat.st_size;
             char *buf = malloc(src_size+1);
             memset(buf, 0, src_size+1);
-			if ((size = readlink(src_path, buf, src_stat.st_size)) > 0)
-			{
-				buf[size] = '\0';
-				if (write(dst_fd, buf, size) != size) {
-					printf("There was a problem while copying data from %s to %s\n", src_path, dst_path);
-				}
-			}
+            if ((size = readlink(src_path, buf, src_size)) > 0)
+            {
+                buf[size] = '\0';
+                if (symlink(buf, dst_path) != 0) {
+                    printf("There was a problem creating a link %s to %s (Errno %d)\n",
+                           dst_path, buf, errno);
+                }
+            }
             if (buf != NULL)
                 free(buf);
-		} else {
+        } else {
             char *buf = malloc(256);
             memset(buf, 0, 256);
-			while ((size = read(src_fd, buf, 256)) > 0)
-			{
+            while ((size = read(src_fd, buf, 256)) > 0)
+            {
                 int nwrite = 0;
-				if ((nwrite = write(dst_fd, buf, size)) != size) {
-					printf("1 There was a problem while copying data from %s to\
-                           %s. Read %d but wrote %d Errno %d\n", src_path, dst_path,
-                           size, nwrite, errno);
-				}
-				memset(buf, 0, 256);
-			}
+                if ((nwrite = write(dst_fd, buf, size)) != size) {
+                    printf("There was a problem while copying data from %s to %s. Read %d but wrote %d Errno %d\n",
+                           src_path, dst_path, size, nwrite, errno);
+                }
+                memset(buf, 0, 256);
+            }
             if (buf != NULL)
                 free(buf);
-		}
-		close(src_fd);
-		close(dst_fd);
-	}
+        }
+        chmod(dst_path, src_stat.st_mode);
+        close(src_fd);
+        close(dst_fd);
+    }
 }
 
 int main(int argc, char** argv) {
 
-	if (argc < NUM_ARGS + 1) {
+    if (argc < NUM_ARGS + 1) {
 
-		printf("Wrong number of args, expected %d, given %d\n", NUM_ARGS, argc - 1);
-		exit(1);
-	}
+        printf("Wrong number of args, expected %d, given %d\n", NUM_ARGS, argc - 1);
+        exit(1);
+    }
 
     struct timeval startTimeVal, shallowTimeVal, deepTimeVal;
     gettimeofday(&startTimeVal, NULL);
 
-	shallowCopy(argv[1], argv[2]);
+    shallowCopy(argv[1], argv[2]);
     gettimeofday(&shallowTimeVal, NULL);
 
-	deepCopy(argv[1], argv[3]);
+    deepCopy(argv[1], argv[3]);
     gettimeofday(&deepTimeVal, NULL);
 
     double start       = (double) startTimeVal.tv_sec   + 1.e-6 * (double)startTimeVal.tv_usec;
     double deepTime    = (double) deepTimeVal.tv_sec    + 1.e-6 *  (double)deepTimeVal.tv_usec;
     double shallowTime = (double) shallowTimeVal.tv_sec + 1.e-6 * (double)shallowTimeVal.tv_usec;
+
     printf("Shallow Copy    = %.4f\n", shallowTime - start);
     printf("Deep Copy       = %.4f\n", deepTime - shallowTime);
-	// TODO: Time how long it takes to execute each copy and print the results.
 
 }
