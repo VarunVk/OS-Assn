8176a77548ca93dd34d216f61c5ed8156ab181b4
VarunVk <vijay094@umn.edu>
2018-04-25 17:26:04 -0500

    Rename the expected binaries to _*

--- discussion13/client.c
+++ discussion13/client.c
@@ -27,25 +27,37 @@ int main(int argc, char** argv) {
 
 	// Create a TCP socket.
 	int sock = socket(AF_INET , SOCK_STREAM , 0);
-	
+
 	// Specify an address to connect to (we use the local host or 'loop-back' address).
 	struct sockaddr_in address;
 	address.sin_family = AF_INET;
 	address.sin_port = htons(SERVER_PORT);
 	address.sin_addr.s_addr = inet_addr(argv[1]);
-	
+
 	// Connect it.
 	if (connect(sock, (struct sockaddr *) &address, sizeof(address)) == 0) {
 
 		// TODO: Write data to socket.
+		char Message[1024];
+		sprintf(Message, "%s %s", argv[2], argv[3]);
+		if (send(sock, Message, strlen(Message), 0) == -1) {
+			perror("Unable to send data to server. Errno: ");
+		}
 
 		// TODO: Read the data sent back.
+		char recvBuf[1024] = {0};
+		ssize_t recvSize = -1;
+		if ((recvSize = read(sock, recvBuf, 1024)) == -1) {
+			perror("Error while receiving response from server. Errno: ");
+		}
+		recvBuf[recvSize] = '\0';
+		printf("Recvd %s\n", recvBuf);
 
 		// Close the socket.
 		close(sock);
-		
+
 	} else {
-	
+
 		perror("Connection failed!");
 	}
 }
--- discussion13/server.c
+++ discussion13/server.c
@@ -14,6 +14,7 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <errno.h>
+#include <semaphore.h>
 
 #define SERVER_PORT 4061
 #define MAX_CONNECTIONS 100
@@ -30,18 +31,31 @@ struct threadArg {
 	double change;
 	int clientfd;
 };
+struct bankAccount *Accounts;
+sem_t sem;
 
-void threadFunction(void* args) {
+void *threadFunction(void* args) {
 
 	struct threadArg* arg = args;
 
 	// TODO: Alter the specified account;
-	
+	struct bankAccount *Ac = Accounts + arg->accountNumber;
+	int balance;
+	pthread_mutex_lock(Ac->mutex);
+	Ac->balance += arg->change;
+	balance = Ac->balance;
+	pthread_mutex_unlock(Ac->mutex);
+
 	// TODO: Write back to client.
-	
+	char Message[1024];
+	sprintf(Message, "%d", balance);
+	if (send(arg->clientfd, Message, strlen(Message), 0) == -1) {
+		perror("Unable to send data to server. Errno: ");
+	}
 	// Close client connection.
 	close(arg->clientfd);
-	
+	sem_post(&sem);
+
 	free(args);
 }
 
@@ -52,38 +66,71 @@ int main(int argc, char** argv) {
 		printf("Wrong number of args, expected %d, given %d\n", NUM_ARGS, argc - 1);
 		exit(1);
 	}
-	
+
 	// Create a TCP socket.
 	int sock = socket(AF_INET , SOCK_STREAM , 0);
-	
+
 	// Bind it to a local address.
 	struct sockaddr_in servAddress;
 	servAddress.sin_family = AF_INET;
 	servAddress.sin_port = htons(SERVER_PORT);
 	servAddress.sin_addr.s_addr = htonl(INADDR_ANY);
 	bind(sock, (struct sockaddr *) &servAddress, sizeof(servAddress));
-	
+
 	// We must now listen on this port.
 	listen(sock, MAX_CONNECTIONS);
-	
+
 	// TODO: Init bank accounts.
-	
+	const int numAccounts = atoi(argv[1]);
+	printf("numAccounts =%d\n", numAccounts);
+	Accounts = calloc(numAccounts, sizeof(struct bankAccount));
+	struct bankAccount *temp = Accounts;
+	for (int i=0; i<numAccounts; i++)
+	{
+		temp->mutex = malloc(sizeof(pthread_mutex_t));
+		if (pthread_mutex_init(temp->mutex, NULL)) {
+			perror("Error: Failed to initialize mutex lock: ");
+			exit(EXIT_FAILURE);
+		}
+		temp++;
+	}
+
 	// TODO: Init threads.
+	int Max_threads = atoi(argv[2]);
+	pthread_t tid[Max_threads];
+	int index = 0;
+	sem_init(&sem, 0, Max_threads);
 
 	// A server typically runs infinitely, with some boolean flag to terminate.
 	while (1) {
-		
+
+		sem_wait(&sem);
 		// Now accept the incoming connections.
 		struct sockaddr_in clientAddress;
-		
+
 		socklen_t size = sizeof(struct sockaddr_in);
 		int clientfd = accept(sock, (struct sockaddr*) &clientAddress, &size);
-		
+
 		// TODO: Read request from the socket.
+        unsigned int recvSize = -1;
+		char recvBuf[1024];
+		if ((recvSize = recv(clientfd, recvBuf, 1024, 0)) > 0) {
+            recvBuf[recvSize] = '\0';
+            printf("Request received %s\n", recvBuf);
+        }
 
 		// TODO: Run a thread to handle the request.
+		char *savePtr;
+		struct threadArg *args = calloc(1, sizeof(struct threadArg));
+		args->accountNumber = atoi(strtok_r(recvBuf, " ", &savePtr));
+		args->change        = atoi(strtok_r(NULL, " ", &savePtr));
+		args->clientfd      = clientfd;
+		printf("Parsed data Ac Num %d change %f\n", args->accountNumber, args->change);
+
+		pthread_create(&tid[index%Max_threads], NULL, threadFunction, args);
+		index++;
 	}
-	
+
 	// Close the socket.
 	close(sock);
 }
