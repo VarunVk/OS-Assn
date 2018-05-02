53f67d3211d94aa073ee49ae6409624815d6830e
VarunVk <vijay094@umn.edu>
2018-05-02 18:32:22 -0500

    Renamed files

---
+++ discussion14/rec14_1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <signal.h>
+
+void foo() {
+	
+	static int count = 0;
+	printf("%d: Count = %d\n", time(NULL), ++count);
+	sleep(1);
+}
+
+int main() {
+	
+	/* Print infinitely. */
+	while (1) foo();
+}
