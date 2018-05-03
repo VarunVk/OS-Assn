8cc783f291e9114df7664ae971d9ef6957c6fd7e
VarunVk <vijay094@umn.edu>
2018-05-02 19:00:16 -0500

    Working now. Added Sig handlers

--- discussion14/rec14_1.c
+++ discussion14/rec14_1.c
@@ -2,14 +2,25 @@
 #include <signal.h>
 
 void foo() {
-	
+
 	static int count = 0;
 	printf("%d: Count = %d\n", time(NULL), ++count);
 	sleep(1);
 }
 
 int main() {
-	
+
+    /* Block SIGINT*/
+    sigset_t old;
+    sigset_t new;
+    sigemptyset(&new);
+    sigaddset(&new, SIGINT);
+
+    if (sigprocmask(SIG_BLOCK, &new, &old) == -1) {
+        perror("Error blocking SIGINT");
+        exit(1);
+    }
+
 	/* Print infinitely. */
 	while (1) foo();
 }
--- discussion14/rec14_2.c
+++ discussion14/rec14_2.c
@@ -1,15 +1,27 @@
 #include <stdio.h>
 #include <signal.h>
 
-void foo() {
-	
-	static int count = 0;
-	printf("%d: Count = %d\n", time(NULL), ++count);
-	sleep(1);
-}
+int KEEP_RUNNING = 1;
 
+void foo() {
+
+	if (KEEP_RUNNING) {
+		static int count = 0;
+		printf("%d: Count = %d\n", time(NULL), ++count);
+		sleep(1);
+	}
+}
+void sigIntHandler(int sig)
+{
+	KEEP_RUNNING = KEEP_RUNNING?0:1;
+}
 int main() {
-	
+
+	// Add handler for SIGINT
+    struct sigaction act;
+    act.sa_handler = sigIntHandler;
+    sigaction(SIGINT, &act, NULL);
+
 	/* Print infinitely. */
 	while (1) foo();
 }
